f44bbc52b6c40d85ba243103e4451e50a536faf1
Ramashankar Kumar <ramashankar.kumar@gridinfocom.com>
2022-08-09 20:24:00 +0530

    error & generics

---
+++ exercise5.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type MyError struct {
+	err string
+}
+
+func (e *MyError) Error() string {
+	return fmt.Sprintf(e.err)
+}
+
+func run() error {
+	return &MyError{"The given number is negative"}
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x > 0 {
+		return math.Sqrt(x), nil
+	} else {
+		return x, run()
+	}
+
+	return 0, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
--- interfaceAndMethod2.go
+++ interfaceAndMethod2.go
@@ -22,9 +22,44 @@ func (v sides) areaOfR() int {
 	return v.length * v.breadth
 }
 
+type point struct {
+	l int
+	b int
+}
+
+func (k *point) areaOfR() int {
+	return k.l * k.b
+}
+
+func describe(i interface{}) {
+	fmt.Printf("(%v, %T)\n", i, i)
+}
+
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Printf("Twice %v is %v\n", v, v*2)
+	case string:
+		fmt.Printf("%q is %v bytes long\n", v, len(v))
+	default:
+		fmt.Printf("I don't know about type %T!\n", v)
+	}
+}
+
 func main() {
 	v := sides{3, 4}
 	fmt.Println("Interface :")
 	fmt.Println("Perimeter of rectangle :", v.perimeterOfR())
 	fmt.Println("Area of rectangle :", v.areaOfR())
+	k := &sides{7, 5}
+	fmt.Println("Area of rectangle Pointer :", k.areaOfR())
+	fmt.Print("Empty Interface :")
+	var i interface{}
+	describe(i)
+	i = "hello"
+	describe(i)
+	fmt.Print("Type Switch :")
+	do(21)
+	do("hello")
+	do(true)
 }
